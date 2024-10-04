d65de32f12f4faa93c098679ec04d3a2a3cd08a9
CityWasTaken <smithfaheed@gmail.com>
2024-10-04 11:46:29 -0400

    Started the HW

---
+++ Develop/index.js
@@ -0,0 +1,27 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {},
+    {}
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    // prompt the user to either create a README file or exit
+
+    // If the choose to create the fil;e, then you prompt them with all the related questions
+
+    // Once you have the answerObj, you pass it to your generateMarkdown function
+
+    const createMarkdown = generateMarkdown();
+
+    writeToFile('README.md', markdown);
+
+}
+
+// Function call to initialize app
+init();
